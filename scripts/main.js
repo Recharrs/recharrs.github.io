6a8887d49690a059dc92141f2f6c44af937a7034
Recharrs <sliz97028@gmail.com>
2017-03-15 10:47:14 +0800

    Add Scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
+function scroll_to_Feature(){
+	$('html, body').animate({scrollTop: $('#Feature-intro').offset().top}, 1000)
+}
 
+function scroll_to_Download(){
+	$('html, body').animate({scrollTop: $('#Downloads').offset().top}, 1000)
+}
 
-function myfunction(){
-    $('html, body').animate({scrollTop: $('#Feature-intro').offset().top}, 1000)
-    return false;
+function scroll_to_About(){
+	$('html, body').animate({scrollTop: $('#About').offset().top}, 1000)
+}
+
+function scroll_to_Contact(){
+	$('html, body').animate({scrollTop: $('#footer').offset().top}, 1000)
 }
