ab731d1a2aafcc96b512e9b7cfe331b463441835
res876ttb <someone10851222@gmail.com>
2017-03-15 22:17:56 +0800

    adjust page and add photos

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 function scroll_to_Feature(){
-	$('html, body').animate({scrollTop: $('#Feature-intro').offset().top}, 1000)
+	$('html, body').animate({scrollTop: $('#Feature-intro').offset().top}, 500);
 }
 
 function scroll_to_Download(){
-	$('html, body').animate({scrollTop: $('#Downloads').offset().top}, 1000)
+	$('html, body').animate({scrollTop: $('#Downloads').offset().top}, 500);
 }
 
 function scroll_to_About(){
-	$('html, body').animate({scrollTop: $('#About').offset().top}, 1000)
+	$('html, body').animate({scrollTop: $('#About').offset().top}, 500);
 }
 
 function scroll_to_Contact(){
-	$('html, body').animate({scrollTop: $('#footer').offset().top}, 1000)
+	$('html, body').animate({scrollTop: $('#footer').offset().top}, 500);
 }
