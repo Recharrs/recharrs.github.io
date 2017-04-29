c987f925b35ad1cbf019ab3123ecdf78e8eca94a
Recharrs <sliz97028@gmail.com>
2017-04-30 01:47:17 +0800

    Add masking

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,37 @@
+$(window).on('load', function() {
+    var $window = $(window),
+        $mask = $('#masking');
+
+    // Disable animations/transitions until the page has loaded.
+    $mask.addClass('is-loading');
+
+    $window.on('load', function() {
+        $window.setTimeout(function() {
+            $mask.removeClass('is-loading');
+        }, 1000);
+    });
+});
+
 function scroll_Top(){
-	$('html, body').animate({scrollTop: 0}, 1000)
+    $('html, body').animate({scrollTop: 0}, 1000)
+}
+
+function scroll_to_Description(){
+    $('html, body').animate({scrollTop: $('#description').offset().top - 55}, 500);
 }
 
 function scroll_to_Feature(){
-	$('html, body').animate({scrollTop: $('#discription').offset().top - 55}, 500);
+    $('html, body').animate({scrollTop: $('#feature').offset().top - 55}, 500);
 }
 
 function scroll_to_Download(){
-	$('html, body').animate({scrollTop: $('#feature').offset().top - 55}, 500);
+    $('html, body').animate({scrollTop: $('#Downloads').offset().top - 55}, 500);
 }
 
 function scroll_to_About(){
-	$('html, body').animate({scrollTop: $('#About').offset().top}, 500);
+    $('html, body').animate({scrollTop: $('#About').offset().top - 55}, 500);
 }
 
 function scroll_to_Contact(){
-	$('html, body').animate({scrollTop: $('#footer').offset().top}, 500);
+    $('html, body').animate({scrollTop: $('#footer').offset().top}, 500);
 }
